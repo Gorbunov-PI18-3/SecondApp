f87351b037e382f848a85a5af0753366663759e2
Gorbunov-PI18-3 <stfouley5@gmail.com>
2020-12-16 08:50:38 +0300

    Добавьте файлы проекта.

---
+++ SecondApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SecondApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "My own name";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
